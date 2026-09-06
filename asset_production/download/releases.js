e3d0b711989319bef5dee26a80c8eb2477a26e18
wisdomzo <zhaoou0427@icloud.com>
2026-09-06 10:43:34 +0900

    v1.2.4, 追加了ASSET发布页

---
+++ asset_production/download/releases.js
@@ -0,0 +1,8 @@
+/* Set a URL to make that release downloadable. Keep null for Coming soon.
+   URLs may be relative to download.html or absolute HTTPS links.
+   Optional version example: 'v2.7.4'. No network request is made here. */
+window.ASSET_RELEASES = {
+  macArm: { url: null, version: null },
+  macIntel: { url: null, version: null },
+  windows: { url: null, version: null }
+};
